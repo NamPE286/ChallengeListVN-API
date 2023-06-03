892ff1300c53af41c20840fac464d4de0ccb3e1a
NamPE286 <nambuihung654@gmail.com>
2023-06-03 22:14:58 +0700

    update

--- src/player/[uid]/GET.js
+++ src/player/[uid]/GET.js
@@ -1,4 +1,5 @@
 require('module-alias/register')
+/** @type {import("@supabase/supabase-js").SupabaseClient} */
 const supabase = require('@config/db')
 
 /** @type {import("express").RequestHandler} */
