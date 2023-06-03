18c8309ac60ef75177593d37aff80b1268af8127
NamPE286 <nambuihung654@gmail.com>
2023-06-03 17:07:47 +0700

    format index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(cors())
 
 
-function *walkSync(dir) {
+function* walkSync(dir) {
   const files = fs.readdirSync(dir, { withFileTypes: true });
   for (const file of files) {
     if (file.isDirectory()) {
@@ -23,24 +23,24 @@ function *walkSync(dir) {
 for (const filePath of walkSync('./src')) {
   var reqPath = './' + filePath.split('\\').join('/').slice(0, -3)
   var route = '/' + reqPath.split('/').slice(2, -1).join('/').replace('[', ':').replace(']', '')
-  if(reqPath.endsWith('GET', reqPath.length)){
+  if (reqPath.endsWith('GET', reqPath.length)) {
     app.get(route, require(reqPath))
   }
-  if(reqPath.endsWith('POST', reqPath.length)){
+  if (reqPath.endsWith('POST', reqPath.length)) {
     app.post(route, require('./middleware/checkUser'), require(reqPath))
   }
-  if(reqPath.endsWith('PUT', reqPath.length)){
+  if (reqPath.endsWith('PUT', reqPath.length)) {
     app.put(route, require('./middleware/checkUser'), require(reqPath))
   }
-  if(reqPath.endsWith('DELETE', reqPath.length)){
+  if (reqPath.endsWith('DELETE', reqPath.length)) {
     app.delete(route, require('./middleware/checkUser'), require(reqPath))
   }
   console.log(`Loaded path ${reqPath} to route ${route}`)
 }
 
 app.listen(
-    process.env.EXPRESS_PORT,
-    () => {
-        console.log(`Server running on http://localhost:${process.env.EXPRESS_PORT}`)
-    }
+  process.env.EXPRESS_PORT,
+  () => {
+    console.log(`Server running on http://localhost:${process.env.EXPRESS_PORT}`)
+  }
 )
