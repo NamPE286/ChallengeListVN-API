1af229df235e4d3a596dc1e51a2cb8093146ca7c
NamPE286 <nambuihung654@gmail.com>
2023-06-03 22:15:03 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
 
 const supabase = require('@supabase/supabase-js').createClient(process.env.API_URL, process.env.API_KEY)
-/** @type {import("@supabase/supabase-js").SupabaseClient} */
 module.exports = supabase
--- src/admin/updateRating/[uid]/PUT.js
+++ src/admin/updateRating/[uid]/PUT.js
@@ -4,12 +4,5 @@ const supabase = require('@config/db')
 
 /** @type {import("express").RequestHandler} */
 module.exports = async (req, res) => {
-    const { id } = req.params
-    const { data, error } = await supabase
-        .from('levels')
-        .select()
-        .eq('id', id)
-        .single()
-    if(error) res.status(500).send(error)
-    else res.send(data)
+    
 }
