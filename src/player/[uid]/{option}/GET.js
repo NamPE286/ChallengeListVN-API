0506e20df814b6d2d2e38cc35000dceeaf1260ca
NamPE286 <nambuihung654@gmail.com>
2023-06-04 15:57:17 +0700

    update

---
+++ src/player/[uid]/{option}/GET.js
@@ -0,0 +1,35 @@
+require('module-alias/register')
+/** @type {import("@supabase/supabase-js").SupabaseClient} */
+const supabase = require('@config/db')
+
+let option = require('@config/defaultOption')
+
+/** @type {import("express").RequestHandler} */
+module.exports = async (req, res) => {
+    if (req.params.option) {
+        const a = JSON.parse(decodeURIComponent(req.params.option))
+        for (const i in a) {
+            option[i] = a[i]
+        }
+    }
+    const { uid } = req.params
+    var resData = {
+        data: {},
+        records: []
+    }
+    var { data, error } = await supabase
+        .from('players')
+        .select()
+        .eq('uid', uid)
+        .single()
+    resData.data = data
+    var { data, error } = await supabase
+        .from('records')
+        .select()
+        .eq('userUID', uid)
+        .eq('accepted', true)
+        .range(option.range.index.start, option.range.index.end)
+    resData.records = data
+    if (error) res.status(500).send(error)
+    else res.send(resData)
+}
