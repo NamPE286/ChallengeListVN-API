045446209a5c987fedfb74ff5648d6e622c93976
NamPE286 <nambuihung654@gmail.com>
2023-06-06 04:43:17 +0700

    add 2 routes

---
+++ src/submit/level/POST.js
@@ -0,0 +1,8 @@
+require('module-alias/register')
+/** @type {import("@supabase/supabase-js").SupabaseClient} */
+const supabase = require('@config/db')
+
+/** @type {import("express").RequestHandler} */
+module.exports = async (req, res) => {
+    res.send({message: 'server ok'})
+}
