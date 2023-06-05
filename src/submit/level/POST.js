53126d1d5f0d1dbabc85f3d6f9aad72b10ed50ae
NamPE286 <nambuihung654@gmail.com>
2023-06-06 05:18:27 +0700

    submit level done

--- src/submit/level/POST.js
+++ src/submit/level/POST.js
@@ -4,5 +4,16 @@ const supabase = require('@config/db')
 
 /** @type {import("express").RequestHandler} */
 module.exports = async (req, res) => {
-    res.send({message: 'server ok'})
+    const submission = req.body
+    var { data, error } = await supabase
+        .from('levels')
+        .insert(submission)
+    if (error) {
+        return res.status(500).send(error)
+    }
+    var { data, error } = await supabase
+        .from('levels')
+        .select()
+        .eq('id', submission.id)
+    res.send(data)
 }
