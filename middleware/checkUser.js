c880494be1fb78da18265d165ccbf434e193358d
NamPE286 <nambuihung654@gmail.com>
2023-06-06 15:31:22 +0700

    update

--- middleware/checkUser.js
+++ middleware/checkUser.js
@@ -6,7 +6,7 @@ const supabase = require('@config/db')
 const jwt = require('jsonwebtoken')
 
 async function getUser(userdata){
-    userdata.user_metadata.avatar_url = userdata.user_metadata.avatar_url.replace('https://lh3.googleusercontent.com/a/', '').replace('w=s96-c', '')
+    userdata.user_metadata.avatar_url = userdata.user_metadata.avatar_url.replace('https://lh3.googleusercontent.com/a/', '').replace('=s96-c', '')
     var avatarID
     var { data, error } = await supabase
         .from('players')
