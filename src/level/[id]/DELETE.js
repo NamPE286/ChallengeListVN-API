0bdc501b71b18f92f5553f3a95c6f0975c100502
NamPE286 <nambuihung654@gmail.com>
2023-06-06 19:29:31 +0700

    DELETE level

--- src/level/[id]/DELETE.js
+++ src/level/[id]/DELETE.js
@@ -1,4 +1,17 @@
+require('module-alias/register')
+/** @type {import("@supabase/supabase-js").SupabaseClient} */
+const supabase = require('@config/db')
+
 /** @type {import("express").RequestHandler} */
 module.exports = async (req, res) => {
+    if (!req.user.isAdmin) {
+        return res.status(403).send()
+    }
+    const { error } = await supabase
+        .from('levels')
+        .delete()
+        .eq('id', req.params.id)
+    console.log(error)
+    if(error) return res.status(500).send(error)
     res.send()
 }
