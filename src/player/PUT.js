67923af3b2f8408a8113b296c6c9861db7ef147f
NamPE286 <nambuihung654@gmail.com>
2023-06-12 04:38:47 +0700

    update

---
+++ src/player/PUT.js
@@ -0,0 +1,17 @@
+require('module-alias/register')
+/** @type {import("@supabase/supabase-js").SupabaseClient} */
+const supabase = require('@config/db')
+
+/** @type {import("express").RequestHandler} */
+module.exports = async (req, res) => {
+    if (!req.user.isAdmin && !req.user.uid == req.body.uid) {
+        return res.status(403).send()
+    }
+    const { error } = await supabase
+        .from('players')
+        .update(req.body)
+        .eq('uid', req.body.uid)
+    console.log(error)
+    if (error) return res.status(500).send(error)
+    res.send(req.body)
+}
