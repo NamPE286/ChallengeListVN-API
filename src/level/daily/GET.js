1864b80c17ff00928a1082480c411bad89045221
NamPE286 <nambuihung654@gmail.com>
2023-06-12 15:38:11 +0700

    test

--- src/level/daily/GET.js
+++ src/level/daily/GET.js
@@ -4,12 +4,14 @@ const supabase = require('@config/db')
 
 /** @type {import("express").RequestHandler} */
 module.exports = async (req, res) => {
-    var start = new Date();
-    start.setUTCHours(0, 0, 0, 0);
-    var { data, error } = await supabase
-        .from('levels')
-        .select('*, players!levels_creatorUID_fkey(*)')
-        .eq('dailyStart', start.toDateString())
-        .single()
-    res.send(data)
+    res.send()
+    // var start = new Date();
+    // start.setUTCHours(0, 0, 0, 0);
+    // var { data, error } = await supabase
+    //     .from('levels')
+    //     .select('*, players!levels_creatorUID_fkey(*)')
+    //     .eq('dailyStart', start.toISOString())
+    //     .single()
+    // if (error) res.status(500).send(error)
+    // else res.send(data)
 }
