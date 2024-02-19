950a0888d30bc798f660a78f71bf2dc8623b94e4
NamPE286 <nambuihung654@gmail.com>
2024-02-19 13:01:06 +0700

    player recent activities

--- src/player/[uid]/{option}/GET.js
+++ src/player/[uid]/{option}/GET.js
@@ -2,6 +2,96 @@ require('module-alias/register')
 /** @type {import("@supabase/supabase-js").SupabaseClient} */
 const supabase = require('@config/db')
 
+async function getRecentActivities(uid) {
+    const res = []
+
+    var { data, error } = await supabase
+        .from('records_view')
+        .select('*, levels!inner(*, players!levels_creatorUID_fkey(*))')
+        .eq('userUID', uid)
+        .eq('accepted', true)
+        .eq('levels.accepted', true)
+        .order('timestamp', { ascending: false })
+        .limit(5)
+
+    for(const i of data) {
+        res.push({
+            type: 'record',
+            data: i
+        })
+    }
+
+    var { data, error } = await supabase
+        .from('levels')
+        .select('*, players!levels_creatorUID_fkey(*))')
+        .eq('creatorUID', uid)
+        .eq('accepted', true)
+        .order('timestamp', { ascending: false })
+        .limit(5)
+
+    for(const i of data) {
+        res.push({
+            type: 'level',
+            data: i
+        })
+    }
+
+    res.sort((a, b) => a.data.timestamp > b.data.timestamp ? 1 : -1)
+
+    if(res.length <= 5) {
+        return res
+    }
+
+    return res.slice(0, 5)
+}
+
+async function getData(uid) {
+    var { data, error } = await supabase
+        .from('players')
+        .select()
+        .eq('uid', uid)
+        .single()
+
+    if(error) {
+        throw error
+    }
+
+    return data
+}
+
+async function getRecords(uid, option) {
+    var { data, error } = await supabase
+        .from('records_view')
+        .select('*, levels!inner(*, players!levels_creatorUID_fkey(*))')
+        .eq('userUID', uid)
+        .eq('accepted', true)
+        .eq('levels.accepted', true)
+        .order('rating', { ascending: false })
+        .range(option.range.index.start, option.range.index.end)
+
+    if(error) {
+        throw error
+    }
+
+    return data
+}
+
+async function getLevels(uid, option) {
+    var { data, error } = await supabase
+        .from('levels')
+        .select('*, players!levels_creatorUID_fkey(*))')
+        .eq('creatorUID', uid)
+        .eq('accepted', true)
+        .order('id', { ascending: false })
+        .range(option.range.index.start, option.range.index.end)
+    
+    if(error) {
+        throw error
+    }
+
+    return data
+}
+
 /** @type {import("express").RequestHandler} */
 module.exports = async (req, res) => {
     let option = JSON.parse(JSON.stringify(require('@config/defaultOption')))
@@ -11,35 +101,13 @@ module.exports = async (req, res) => {
             option[i] = a[i]
         }
     }
+
     const { uid } = req.params
-    var resData = {
-        data: {},
-        records: [],
-        levels: []
-    }
-    var { data, error } = await supabase
-        .from('players')
-        .select()
-        .eq('uid', uid)
-        .single()
-    resData.data = data
-    var { data, error } = await supabase
-        .from('records_view')
-        .select('*, levels!inner(*, players!levels_creatorUID_fkey(*))')
-        .eq('userUID', uid)
-        .eq('accepted', true)
-        .eq('levels.accepted', true)
-        .order('rating', { ascending: false })
-        .range(option.range.index.start, option.range.index.end)
-    resData.records = data
-    var { data, error } = await supabase
-        .from('levels')
-        .select('*, players!levels_creatorUID_fkey(*))')
-        .eq('creatorUID', uid)
-        .eq('accepted', true)
-        .order('id', { ascending: false })
-        .range(option.range.index.start, option.range.index.end)
-    resData.levels = data
-    if (error) res.status(500).send(error)
-    else res.send(resData)
+
+    res.send({
+        recentActivities: await getRecentActivities(uid),
+        data: await getData(uid),
+        records: await getRecords(uid, option),
+        levels: await getLevels(uid, option)
+    })
 }
